35256eb51dd7327ee994a1aab71fd7a18a20e378
Skwanky <BlauerSkwanky@aol.com>
2012-04-22 23:00:46 +0200

    not empty message

---
+++ include/Player.h
@@ -0,0 +1,13 @@
+#ifndef __Player_H_INCLUDED__
+#define __Player_H_INCLUDED__
+#include <Genie.h>
+class Player
+{
+  public:
+	Player(Genie& _genie);	
+  private:	
+	Genie& _genie;
+	
+	
+};
+#endif
---
+++ src/Player.cpp
@@ -0,0 +1,7 @@
+#include <Player.h>
+
+Player::Player(Genie& genie):_genie(genie)
+{
+	
+	
+}	
