299e87daf02d87d7b021443c0a443ac33b1d8b51
Marcus Hoffmann <bubuiic@aol.com>
2012-04-24 00:54:06 +0200

    door..

--- include/tile.h
+++ include/tile.h
@@ -11,8 +11,10 @@ class Tile
 		float height;
 		Vector3f color;
 		inline Tile(std::string Type, Vector3f color):type(type),color(color){}
-		inline Tile():type(0),color(Vector3f(0.5,0.5,0.5)),height(1.0),blocking(false){}
+		inline Tile():type(0),color(Vector3f(0.5,0.5,0.5)),height(1.0),blocking(false),item(-1),doorid(-1){}
 		bool blocking;
+		int item;
+		int doorid;
 	private:
 
 };
